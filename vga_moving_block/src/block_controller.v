b1c569e4da5f942ac639801981dcf5fbf5cc27b5
Yash Gupta <yygupta@usc.edu>
2025-11-28 23:33:27 -0800

    updated 28Nov-1

--- vga_moving_block/src/block_controller.v
+++ vga_moving_block/src/block_controller.v
@@ -1,214 +1,257 @@
 `timescale 1ns / 1ps
 
 module block_controller(
-	input clk, //this clock must be a slow enough clock to view the changing positions of the objects
-	//input mastclk; // ASK SHRI IF THIS IS REQUIRED? 
-	input bright,
-	input rst,
-	input up, input down, input left, input right,
-	input [9:0] hCount, vCount,
-	output reg [11:0] rgb,
-	output reg [11:0] background
-   );
-	wire block_fill;
-	
-	//these two values dictate the center of the block, incrementing and decrementing them leads the block to move in certain directions
-	reg [9:0] xpos, ypos;
-	
-	parameter RED   = 12'b1111_0000_0000;
-	// instantiate the various blocks
-	// ROM instances for each tile
-	
-	wire [11:0] rom_data_2, rom_data_4, rom_data_8, rom_data_16;
-	wire [11:0] rom_data_32, rom_data_64, rom_data_128, rom_data_256;
-	wire [11:0] rom_data_512, rom_data_1024, rom_data_2048;
-
-	// Assuming each tile is 30x30 pixels
-	wire [9:0] rom_addr;
-
-	// Instantiate all tile ROM modules
-    tile_rom_2 rom_inst_2 (
-        .addr(rom_addr),
-        .data(rom_data_2)
-    );
-    
-    tile_rom_4 rom_inst_4 (
-        .addr(rom_addr),
-        .data(rom_data_4)
-    );
-    
-    tile_rom_8 rom_inst_8 (
-        .addr(rom_addr),
-        .data(rom_data_8)
-    );
-    
-    tile_rom_16 rom_inst_16 (
-        .addr(rom_addr),
-        .data(rom_data_16)
-    );
-    
-    tile_rom_32 rom_inst_32 (
-        .addr(rom_addr),
-        .data(rom_data_32)
-    );
-    
-    tile_rom_64 rom_inst_64 (
-        .addr(rom_addr),
-        .data(rom_data_64)
-    );
-    
-    tile_rom_128 rom_inst_128 (
-        .addr(rom_addr),
-        .data(rom_data_128)
-    );
-    
-    tile_rom_256 rom_inst_256 (
-        .addr(rom_addr),
-        .data(rom_data_256)
-    );
-    
-    tile_rom_512 rom_inst_512 (
-        .addr(rom_addr),
-        .data(rom_data_512)
-    );
-    
-    tile_rom_1024 rom_inst_1024 (
-        .addr(rom_addr),
-        .data(rom_data_1024)
-    );
-    
-    tile_rom_2048 rom_inst_2048 (
-        .addr(rom_addr),
-        .data(rom_data_2048)
-    );
-
-
-    // Grid parameters
+    input clk,
+    input mastClk,
+    input bright,
+    input rst,
+    input up, input down, input left, input right,
+    input [9:0] hCount, vCount,
+    output reg [11:0] rgb,
+    output reg [11:0] background,
+    output wire [15:0] score_out
+);
+   
     parameter TILE_SIZE = 30;
-    parameter TILE_GAP = 5;
-    parameter GRID_START_X = 200;
-    parameter GRID_START_Y = 100;
+    parameter GAP_SIZE = 4;
+    parameter GRID_START_X = 400;
+    parameter GRID_START_Y = 200;
+    parameter BORDER_THICKNESS = 8;  // NEW: Border width in pixels
+   
+    wire [79:0] board_flat;
+    wire [15:0] game_score;
+    wire game_over;
+    wire game_won;
+    wire [1:0] cursor_col;
+    wire [4:0] spawn_val;
+    wire display_ready;
     
-    // Determine which grid cell (0-3 for x and y)
-    wire [1:0] grid_x, grid_y;
-    wire [4:0] pixel_x, pixel_y;  // Position within the tile (0-29)
-    wire in_tile;
+    reg [79:0] board_flat_sync;
+    reg [79:0] board_flat_display;
+    reg [15:0] game_score_sync;
+    reg game_over_sync;
+    reg game_won_sync;
+    reg [1:0] cursor_col_sync;
+    reg [4:0] spawn_val_sync;
+    reg [4:0] spawn_val_display;
+    reg display_ready_sync;
     
-    // Calculate grid position
-    assign grid_x = (hCount - GRID_START_X) / (TILE_SIZE + TILE_GAP);
-    assign grid_y = (vCount - GRID_START_Y) / (TILE_SIZE + TILE_GAP);
-    
-    // Calculate position within current tile
-    assign pixel_x = (hCount - GRID_START_X) % (TILE_SIZE + TILE_GAP);
-    assign pixel_y = (vCount - GRID_START_Y) % (TILE_SIZE + TILE_GAP);
-    
-    // Check if we're inside a tile (not in gap)
-    assign in_tile = (pixel_x < TILE_SIZE) && (pixel_y < TILE_SIZE);
-    
-    // ROM address: row * width + column
-    assign rom_addr = pixel_y * TILE_SIZE + pixel_x;
+    always @(posedge mastClk or posedge rst) begin
+        if (rst) begin
+            board_flat_sync <= 80'd0;
+            board_flat_display <= 80'd0;
+            game_score_sync <= 16'd0;
+            game_over_sync <= 1'b0;
+            game_won_sync <= 1'b0;
+            cursor_col_sync <= 2'd0;
+            spawn_val_sync <= 5'd1;
+            spawn_val_display <= 5'd1;
+            display_ready_sync <= 1'b1;
+        end else begin
+            board_flat_sync <= board_flat;
+            game_score_sync <= game_score;
+            game_over_sync <= game_over;
+            game_won_sync <= game_won;
+            cursor_col_sync <= cursor_col;
+            spawn_val_sync <= spawn_val;
+            display_ready_sync <= display_ready;
+            
+            if (display_ready) begin
+                board_flat_display <= board_flat_sync;
+                spawn_val_display <= spawn_val_sync;
+            end
+        end
+    end
+   
+    wire [4:0] grid [0:3][0:3];
+    genvar r, c;
+    generate
+        for (r = 0; r < 4; r = r + 1) begin : row_gen
+            for (c = 0; c < 4; c = c + 1) begin : col_gen
+                assign grid[r][c] = board_flat_display[((r*4 + c)*5) +: 5];
+            end
+        end
+    endgenerate
+   
+    tetris_2048_core game_core (
+        .clk(mastClk),
+        .rst(rst),
+        .btn_l(left),
+        .btn_r(right),
+        .btn_drop(down),
+        .board_flat(board_flat),
+        .score(game_score),
+        .game_over(game_over),
+        .game_won(game_won),
+        .cursor_col(cursor_col),
+        .spawn_val(spawn_val),
+        .display_ready(display_ready)
+    );
+   
+    assign score_out = game_score_sync;
+   
+    wire [11:0] rom_data_2, rom_data_4, rom_data_8, rom_data_16;
+    wire [11:0] rom_data_32, rom_data_64, rom_data_128, rom_data_256;
+    wire [11:0] rom_data_512, rom_data_1024, rom_data_2048;
+    wire [4:0] rom_row, rom_col;
 
-	    // Game state: 4x4 grid of tile values
-    reg [3:0] grid [0:3][0:3];  // 0=empty, 1=2, 2=4, 3=8, etc.
+    tile_rom_2    tile_2    (.clk(mastClk), .row(rom_row), .col(rom_col), .color_data(rom_data_2));
+    tile_rom_4    tile_4    (.clk(mastClk), .row(rom_row), .col(rom_col), .color_data(rom_data_4));
+    tile_rom_8    tile_8    (.clk(mastClk), .row(rom_row), .col(rom_col), .color_data(rom_data_8));
+    tile_rom_16   tile_16   (.clk(mastClk), .row(rom_row), .col(rom_col), .color_data(rom_data_16));
+    tile_rom_32   tile_32   (.clk(mastClk), .row(rom_row), .col(rom_col), .color_data(rom_data_32));
+    tile_rom_64   tile_64   (.clk(mastClk), .row(rom_row), .col(rom_col), .color_data(rom_data_64));
+    tile_rom_128  tile_128  (.clk(mastClk), .row(rom_row), .col(rom_col), .color_data(rom_data_128));
+    tile_rom_256  tile_256  (.clk(mastClk), .row(rom_row), .col(rom_col), .color_data(rom_data_256));
+    tile_rom_512  tile_512  (.clk(mastClk), .row(rom_row), .col(rom_col), .color_data(rom_data_512));
+    tile_rom_1024 tile_1024 (.clk(mastClk), .row(rom_row), .col(rom_col), .color_data(rom_data_1024));
+    tile_rom_2048 tile_2048 (.clk(mastClk), .row(rom_row), .col(rom_col), .color_data(rom_data_2048));
+
+    // Grid dimensions
+    wire [9:0] GRID_WIDTH = 4*TILE_SIZE + 5*GAP_SIZE;
+    wire [9:0] GRID_HEIGHT = 4*TILE_SIZE + 5*GAP_SIZE;
     
-    // Get current tile value at this grid position
-    wire [3:0] current_tile_value;
-    assign current_tile_value = grid[grid_y][grid_x];
+    // NEW: Border detection
+    wire in_border;
+    assign in_border = (hCount >= GRID_START_X - BORDER_THICKNESS && 
+                       hCount < GRID_START_X + GRID_WIDTH + BORDER_THICKNESS &&
+                       vCount >= GRID_START_Y - BORDER_THICKNESS && 
+                       vCount < GRID_START_Y + GRID_HEIGHT + BORDER_THICKNESS) &&
+                      !(hCount >= GRID_START_X && 
+                        hCount < GRID_START_X + GRID_WIDTH &&
+                        vCount >= GRID_START_Y && 
+                        vCount < GRID_START_Y + GRID_HEIGHT);
+
+    // Pipeline stage
+    wire [9:0] rel_x, rel_y;
+    reg [1:0] grid_col_stage1, grid_row_stage1;
+    reg [9:0] cell_pos_x_stage1, cell_pos_y_stage1;
+    reg in_grid_area_stage1;
+    reg in_border_stage1;  // NEW: Register border signal
+    reg [9:0] hCount_stage1, vCount_stage1;
     
-    // Multiplexer: select ROM data based on tile value
-    reg [11:0] selected_rom_data;
+    assign rel_x = hCount - GRID_START_X;
+    assign rel_y = vCount - GRID_START_Y;
+    
+    wire in_grid_area_comb;
+    assign in_grid_area_comb = (hCount >= GRID_START_X) &&
+                               (hCount < GRID_START_X + GRID_WIDTH) &&
+                               (vCount >= GRID_START_Y) &&
+                               (vCount < GRID_START_Y + GRID_HEIGHT);
+    
+    always @(posedge mastClk) begin
+        grid_col_stage1 <= (rel_x) / (TILE_SIZE + GAP_SIZE);
+        grid_row_stage1 <= (rel_y) / (TILE_SIZE + GAP_SIZE);
+        cell_pos_x_stage1 <= rel_x % (TILE_SIZE + GAP_SIZE);
+        cell_pos_y_stage1 <= rel_y % (TILE_SIZE + GAP_SIZE);
+        in_grid_area_stage1 <= in_grid_area_comb;
+        in_border_stage1 <= in_border;  // NEW: Register border
+        hCount_stage1 <= hCount;
+        vCount_stage1 <= vCount;
+    end
+    
+    wire [1:0] grid_col_use, grid_row_use;
+    wire [5:0] tile_x, tile_y;
+    wire in_gap, in_tile;
+    
+    assign grid_col_use = grid_col_stage1;
+    assign grid_row_use = grid_row_stage1;
+    assign in_gap = (cell_pos_x_stage1 < GAP_SIZE) || (cell_pos_y_stage1 < GAP_SIZE);
+    assign in_tile = in_grid_area_stage1 && !in_gap;
+    assign tile_x = cell_pos_x_stage1 - GAP_SIZE;
+    assign tile_y = cell_pos_y_stage1 - GAP_SIZE;
+   
+    wire [4:0] current_tile;
+    assign current_tile = grid[grid_row_use][grid_col_use];
+   
+    // Spawn tile preview
+    wire in_cursor_area;
+    wire [9:0] preview_offset_x, preview_offset_y;
+    wire [5:0] preview_tile_x, preview_tile_y;
+    wire in_preview_tile;
+
+    assign in_cursor_area = (grid_col_use == cursor_col_sync) && 
+                            (vCount_stage1 >= GRID_START_Y - 40) &&
+                            (vCount_stage1 < GRID_START_Y - 10) &&
+                            (hCount_stage1 >= GRID_START_X + GAP_SIZE + cursor_col_sync * (TILE_SIZE + GAP_SIZE)) &&
+                            (hCount_stage1 < GRID_START_X + GAP_SIZE + cursor_col_sync * (TILE_SIZE + GAP_SIZE) + TILE_SIZE);
+
+    assign preview_offset_x = hCount_stage1 - (GRID_START_X + GAP_SIZE + cursor_col_sync * (TILE_SIZE + GAP_SIZE));
+    assign preview_offset_y = vCount_stage1 - (GRID_START_Y - 40);
+    assign preview_tile_x = preview_offset_x[5:0];
+    assign preview_tile_y = preview_offset_y[5:0];
+
+    assign in_preview_tile = in_cursor_area && 
+                             (preview_tile_y < TILE_SIZE) && 
+                             (preview_tile_x < TILE_SIZE) &&
+                             display_ready_sync;
+   
+    wire [4:0] active_rom_row, active_rom_col;
+    assign active_rom_row = in_preview_tile ? preview_tile_y[4:0] : tile_y[4:0];
+    assign active_rom_col = in_preview_tile ? preview_tile_x[4:0] : tile_x[4:0];
+    assign rom_row = active_rom_row;
+    assign rom_col = active_rom_col;
+   
+    reg [11:0] selected_tile_color;
     always @(*) begin
-        case(current_tile_value)
-            4'd0:  selected_rom_data = 12'hCDC1B4;  // Empty tile color - modify? to black?
-            4'd1:  selected_rom_data = rom_data_2;
-            4'd2:  selected_rom_data = rom_data_4;
-            4'd3:  selected_rom_data = rom_data_8;
-            4'd4:  selected_rom_data = rom_data_16;
-            4'd5:  selected_rom_data = rom_data_32;
-            4'd6:  selected_rom_data = rom_data_64;
-            4'd7:  selected_rom_data = rom_data_128;
-            4'd8:  selected_rom_data = rom_data_256;
-            4'd9:  selected_rom_data = rom_data_512;
-            4'd10: selected_rom_data = rom_data_1024;
-            4'd11: selected_rom_data = rom_data_2048;
-            default: selected_rom_data = 12'h000;
+        case(current_tile)
+            5'd0:  selected_tile_color = 12'hCCC;
+            5'd1:  selected_tile_color = rom_data_2;
+            5'd2:  selected_tile_color = rom_data_4;
+            5'd3:  selected_tile_color = rom_data_8;
+            5'd4:  selected_tile_color = rom_data_16;
+            5'd5:  selected_tile_color = rom_data_32;
+            5'd6:  selected_tile_color = rom_data_64;
+            5'd7:  selected_tile_color = rom_data_128;
+            5'd8:  selected_tile_color = rom_data_256;
+            5'd9:  selected_tile_color = rom_data_512;
+            5'd10: selected_tile_color = rom_data_1024;
+            5'd11: selected_tile_color = rom_data_2048;
+            default: selected_tile_color = 12'h000;
         endcase
     end
-
-	// Final RGB output
-	/*when outputting the rgb value in an always block like this, make sure to include the if(~bright) statement, as this ensures the monitor 
-	will output some data to every pixel and not just the images you are trying to display*/
+    
+    reg [11:0] preview_tile_color;
+    always @(*) begin
+        case(spawn_val_display)
+            5'd1:  preview_tile_color = rom_data_2;
+            5'd2:  preview_tile_color = rom_data_4;
+            5'd3:  preview_tile_color = rom_data_8;
+            5'd4:  preview_tile_color = rom_data_16;
+            5'd5:  preview_tile_color = rom_data_32;
+            5'd6:  preview_tile_color = rom_data_64;
+            5'd7:  preview_tile_color = rom_data_128;
+            5'd8:  preview_tile_color = rom_data_256;
+            5'd9:  preview_tile_color = rom_data_512;
+            5'd10: preview_tile_color = rom_data_1024;
+            5'd11: preview_tile_color = rom_data_2048;
+            default: preview_tile_color = 12'h000;
+        endcase
+    end
+   
+    // RGB output with border support
     always @(*) begin
         if (~bright)
-            rgb = 12'h000;  // Black outside display area
-        else if (in_tile && (hCount >= GRID_START_X) && 
-                 (hCount < GRID_START_X + 4*(TILE_SIZE + TILE_GAP)) &&
-                 (vCount >= GRID_START_Y) && 
-                 (vCount < GRID_START_Y + 4*(TILE_SIZE + TILE_GAP)))
-            rgb = selected_rom_data;  // Display tile from ROM
+            rgb = 12'h000;
+        else if (in_border_stage1 && game_won_sync)  // NEW: Green border for win
+            rgb = 12'h0F0;
+        else if (in_border_stage1 && game_over_sync)  // NEW: Red border for lose
+            rgb = 12'hF00;
+        else if (in_preview_tile)
+            rgb = preview_tile_color;
+        else if (in_gap && in_grid_area_stage1)
+            rgb = 12'hFFF;
+        else if (in_tile)
+            rgb = selected_tile_color;
         else
-            rgb = background;  // Background color
+            rgb = background;
+    end
+   
+    always @(posedge mastClk or posedge rst) begin
+        if (rst)
+            background <= 12'h000;
+        else
+            background <= 12'h000;
     end
 
-		//the +-5 for the positions give the dimension of the block (i.e. it will be 10x10 pixels)
-	assign block_fill=vCount>=(ypos-15) && vCount<=(ypos+15) && hCount>=(xpos-15) && hCount<=(xpos+15); // changed to be 30*30 block at the centre for now 
-	// for a certain (xpos, ypos) , a certain radius is assigned 1: effectively selecting the block
-
-	// ACTUAL MOVEMENT
-	always@(posedge clk, posedge rst) 
-	begin
-		if(rst)
-		begin 
-			//rough values for center of screen
-			xpos<=450;
-			ypos<=250;
-		end
-		else if (clk) begin
-		
-		/* Note that the top left of the screen does NOT correlate to vCount=0 and hCount=0. The display_controller.v file has the 
-			synchronizing pulses for both the horizontal sync and the vertical sync begin at vcount=0 and hcount=0. Recall that after 
-			the length of the pulse, there is also a short period called the back porch before the display area begins. So effectively, 
-			the top left corner corresponds to (hcount,vcount)~(144,35). Which means with a 640x480 resolution, the bottom right corner 
-			corresponds to ~(783,515).  
-		*/
-			if(right) begin
-				xpos<=xpos+2; //change the amount you increment to make the speed faster 
-				if(xpos==800) //these are rough values to attempt looping around, you can fine-tune them to make it more accurate- refer to the block comment above
-					xpos<=150;
-			end
-			else if(left) begin
-				xpos<=xpos-2;
-				if(xpos==150)
-					xpos<=800;
-			end
-			else if(up) begin
-				ypos<=ypos-2;
-				if(ypos==34)
-					ypos<=514;
-			end
-			else if(down) begin
-				ypos<=ypos+2;
-				if(ypos==514)
-					ypos<=34;
-			end
-		end
-	end
-	
-	//the background color reflects the most recent button press
-	always@(posedge clk, posedge rst) begin
-		if(rst)
-			background <= 12'b1111_1111_1111;
-		else 
-			if(right)
-				background <= 12'b1111_1111_0000;
-			else if(left)
-				background <= 12'b0000_1111_1111;
-			else if(down)
-				background <= 12'b0000_1111_0000;
-			else if(up)
-				background <= 12'b0000_0000_1111;
-	end
-
-	
-	
 endmodule
